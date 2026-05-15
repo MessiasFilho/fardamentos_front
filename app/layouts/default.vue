8e0c44ef6de7bdb870d83dcb96cbcccfaf95ddc3
MessiasFilho <messiasfilho98@gmail.com>
2026-05-15 13:46:27 -0300

    primeiro commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+    <header>
+            <h1>Fardamentos</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+    
+    </header>
+    
+
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Fardamentos</h1>
+        <p>Fardamentos para la venta</p>
+        <p>Fardamentos para la venta</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
